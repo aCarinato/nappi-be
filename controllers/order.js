3388e3a5e4065ccdb17c520223bf33f4c2d1d82b
aCarinato <alessandro.carinato@gmail.com>
2022-08-08 15:49:13 +0200

    is paid

--- controllers/order.js
+++ controllers/order.js
@@ -36,25 +36,50 @@ export const getOrder = async (req, res) => {
 export const payOrder = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
+
     if (order) {
       if (order.isPaid) {
         return res.json({ message: 'Error: order is already paid' });
       }
       order.isPaid = true;
       order.paidAt = Date.now();
-      // the following are data provided by paypal
-      order.paymentResult = {
-        id: req.body.id,
-        status: req.body.status,
-        email_address: req.body.email_address,
-      };
+
       const paidOrder = await order.save();
       res.json({ message: 'order paid successfully', order: paidOrder });
     } else {
       res.json({ message: 'Error: order not found' });
     }
   } catch (err) {
-    res.status(404); // if no specific status is set, by default it would fall back to 500
-    return next(err); // this error is passed to the middleware that handles the error
+    res.status(404);
+    return next(err);
   }
 };
+
+// @desc    Pay an order
+// @route   PUT /api/order/:id/pay
+// @access  Private
+// export const payOrder = async (req, res) => {
+//   try {
+//     const order = await Order.findById(req.params.id);
+//     if (order) {
+//       if (order.isPaid) {
+//         return res.json({ message: 'Error: order is already paid' });
+//       }
+//       order.isPaid = true;
+//       order.paidAt = Date.now();
+//       // the following are data provided by paypal
+//       order.paymentResult = {
+//         id: req.body.id,
+//         status: req.body.status,
+//         email_address: req.body.email_address,
+//       };
+//       const paidOrder = await order.save();
+//       res.json({ message: 'order paid successfully', order: paidOrder });
+//     } else {
+//       res.json({ message: 'Error: order not found' });
+//     }
+//   } catch (err) {
+//     res.status(404); // if no specific status is set, by default it would fall back to 500
+//     return next(err); // this error is passed to the middleware that handles the error
+//   }
+// };
