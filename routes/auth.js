4c629a0013beddcbead35deafa5876605a5c7233
aCarinato <alessandro.carinato@gmail.com>
2022-08-09 13:59:13 +0200

    update profile

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,18 @@ import express from 'express';
 const router = express.Router();
 
 import { requireSignin } from '../middlewares/checkAuth.js';
-import { signup, login, currentUser, getUser } from '../controllers/auth.js';
+import {
+  signup,
+  login,
+  currentUser,
+  getUser,
+  updateProfile,
+} from '../controllers/auth.js';
 
 router.get('/current-user', requireSignin, currentUser);
 router.get('/:email', getUser);
 router.post('/signup', signup);
 router.post('/login', login);
+router.put('/update', requireSignin, updateProfile);
 
 export default router;
